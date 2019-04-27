4581d03b47a9a5424ea2c6efd9f8e2f3ba571d71
yuzhangoscar <yuzhang2002@gmail.com>
2019-04-27 22:38:31 +1000

    added lots of stuff

--- memory_webpage/blog/views.py
+++ memory_webpage/blog/views.py
@@ -1,8 +1,23 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+
+posts = [
+    {
+        'author': 'YZ',
+        'title': 'First post',
+        'content': 'R18'
+    },
+    {
+        'author': 'YZ 2.0',
+        'title': 'Second post',
+        'content': 'content is hot'
+    }
+]
 
 def home(request):
-	return HttpResponse('<h1>Blog Home</h1>')
+	context = {
+	    'posts': posts
+	}
+	return render(request, 'blog/home.html', context)
 
 def about(request):
-	return HttpResponse('<h1>Blog About</h1>')
+	return render(request, 'blog/about.html', {'title':'about about'})

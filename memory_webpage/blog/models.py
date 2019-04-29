c2458694371d5cf2dfe2ef5a93cc5aaf6a3b75e7
yuzhangoscar <yuzhang2002@gmail.com>
2019-04-29 21:35:53 +1000

    added model

--- memory_webpage/blog/models.py
+++ memory_webpage/blog/models.py
@@ -1,3 +1,13 @@
 from django.db import models
-
+from django.utils import timezone
+from django.contrib.auth.models import User
 # Create your models here.
+
+class Task(models.Model):
+	task = models.CharField(max_length = 100, primary_key = True)
+	studied_date = models.DateTimeField(default = timezone.now)
+	first_review_date = models.DateTimeField(default = timezone.now)
+	second_review_date = models.DateTimeField(default = timezone.now)
+	third_review_date = models.DateTimeField(default = timezone.now)
+	fourth_review_date = models.DateTimeField(default = timezone.now)
+	fifth_review_date = models.DateTimeField(default = timezone.now)

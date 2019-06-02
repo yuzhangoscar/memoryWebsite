840a72b5735461284790bdbfb4887338450b36af
yuzhangoscar <yuzhang2002@gmail.com>
2019-06-02 23:23:35 +1000

    WIP

--- memory_webpage/blog/admin.py
+++ memory_webpage/blog/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from .models import Task
 
 # Register your models here.
+@admin.register(Task)
+class TaskAdmin(admin.ModelAdmin):
+	list_display = ('task', 'studied_date', 'first_review_date')
+	list_filter = ('task', 'studied_date')
+	search_fields = ('task', 'studied_date')
+	date_hierarchy = 'studied_date'
+	ordering = ('task', 'studied_date')

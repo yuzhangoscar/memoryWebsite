228642fb4e35187ca5836e5e06bddd45ba74865a
yuzhangoscar <yuzhang2002@gmail.com>
2019-06-13 07:01:29 +1000

    WIP

--- memory_webpage/blog/urls.py
+++ memory_webpage/blog/urls.py
@@ -1,8 +1,12 @@
-from django.contrib import admin
-from django.urls import path, include
-from . import views
+from django.urls import path
+from . import views 
 
+app_name = 'blog'
 urlpatterns = [
-    path('', views.post_list, name='post_list'),
-    path('<int:year>/<int:month>/<int:day>/<slug:post>/', views.post_detail, name='post_detail'),
-]
+    # post views
+    # path('', views.post_list, name='post_list'),
+    path('', views.PostListView.as_view(), name='post_list'),
+    path('<int:year>/<int:month>/<int:day>/<slug:post>/',
+         views.post_detail,
+         name='post_detail'),
+] 
